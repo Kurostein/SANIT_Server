8d82bf6b050fa8304d1b023defbc68f437c650fe
Felipe Rastelli <felipe-rastelli@hotmail.com>
2015-10-03 23:03:36 -0300

    Little tests with android app connection.

--- src/br/com/lorencity/servlet/ServletUsuario.java
+++ src/br/com/lorencity/servlet/ServletUsuario.java
@@ -1,6 +1,7 @@
 package br.com.lorencity.servlet;
 
 import java.io.IOException;
+import java.io.PrintStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -22,6 +23,7 @@ public class ServletUsuario extends HttpServlet {
     /**
      * @see HttpServlet#HttpServlet()
      */
+
     public ServletUsuario() {
         super();
         // TODO Auto-generated constructor stub
@@ -40,16 +42,29 @@ public class ServletUsuario extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
+		/*
 		DadosDenuncia dadosDenuncia = ModelGenerator.preencherModelo(request);
 		String action = request.getParameter("action");
 		
 		BoUsuario boUsuario = new BoUsuario();
 		boUsuario.doServletAction(dadosDenuncia, action);
-		
+		*/
 		//O usuário apenas insere dados, não necessita receber o retorno do método.
+		/*
+		String cpf = request.getParameter("cpf");
+		String tipo = request.getParameter("tipoProblema");
+		String endereco = request.getParameter("endereco");
+		String cep = request.getParameter("cep");
 		
+		System.out.println(cpf+tipo+endereco+cep);
+		*/
+		System.out.println("Teste: "+request.getParameter("hello"));
 		
+		response.setContentType("application/text");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().write("Enviado");
+		
+		//request.getParameterMap() retorna um Map<String, String>
 	}
 
 }
